cc06b7ecc57298c9e5dd3c9b654d912390449725
busster <jasonmbuss@gmail.com>
2017-08-24 17:47:41 -0500

    WIP

--- src/components/createItem.js
+++ src/components/createItem.js
@@ -2,10 +2,12 @@
   const html = `
   <div class="create-item">
     <input class="item-input" v-model="item" placeholder="Add item to todolist">
-    <div v-if="showActions" class="buttons">
-      <a v-on:click.stop.prevent="addItem()" class="item-button add">Add Item</a>
-      <a v-on:click.stop.prevent="reset()" class="item-button clear">Clear</a>
-    </div>
+    <transition name="fade">
+      <div v-if="showActions" class="buttons">
+        <a v-on:click.stop.prevent="addItem()" class="item-button add">Add Item</a>
+        <a v-on:click.stop.prevent="reset()" class="item-button clear">Clear</a>
+      </div>
+    </transition>
   </div>
   `
 
